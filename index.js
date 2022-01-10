f5b1374433d7d65f3314e6c01df15257d9afcea5
Chaillot Valentin <valentin.chaillot@etu.umontpellier.fr>
2022-01-10 08:47:37 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 5000 // this is very important
+
+app.get('/recettes', function (req, res) {
+  res.send('Hello World!')
+})
+
+
+
+
+
+app.listen(PORT, function () {
+  console.log('Example app listening on port ' + PORT)
+})
