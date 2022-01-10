527335ea4b0b21940a93849313fd4ee99fe6ead4
Chaillot Valentin <valentin.chaillot@etu.umontpellier.fr>
2022-01-10 10:05:01 +0100

    Route post et delete

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ const app = express()
 const PORT = process.env.PORT || 5000 // this is very important
 const axios = require('axios');
 
+//Route qui récupère la liste des recettes
 app.get('/recettes', function (req, res) {
   const fetchUser1 = axios.get('https://tpnote-d015.restdb.io/rest/recettes',  {headers:
      { 'x-apikey': '70f9440ef523be720647499c94730c2d429f8'
      }})
-  .then(result => res.send(result.data))
+  .then(result => res.json(result.data))
 })
 
+//Route qui recupère une recettes
 app.get('/recettes/:id', function (req, res) {
   const fetchUser1 = axios.get('https://tpnote-d015.restdb.io/rest/recettes/'+ req.params.id,  {headers:
      { 'x-apikey': '70f9440ef523be720647499c94730c2d429f8'
@@ -17,6 +19,25 @@ app.get('/recettes/:id', function (req, res) {
   .then(result => res.json(result.data))
 })
 
+//Route qui ajoute une recettes
+app.post('/recettes', function (req, res) {
+  const obj = {
+    nom: "test",
+    ingredients: "test"
+  }
+  const fetchUser1 = axios.post('https://tpnote-d015.restdb.io/rest/recettes',obj,{headers:
+     { 'x-apikey': '70f9440ef523be720647499c94730c2d429f8'
+   }})
+  .then(result => res.json(result.data))
+})
+
+//Route qui supprime une recettes
+app.delete('/recettes/:id', function (req, res) {
+    const fetchUser1 = axios.delete('https://tpnote-d015.restdb.io/rest/recettes/'+ req.params.id,  {headers:
+            { 'x-apikey': '70f9440ef523be720647499c94730c2d429f8'
+            }})
+        .then(result => res.json(result.data))
+})
 
 app.listen(PORT, function () {
   console.log('Example app listening on port ' + PORT)
