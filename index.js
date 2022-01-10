91c94790fa70dda7b7a7be9780e6d1e36f88d79e
Chaillot Valentin <valentin.chaillot@etu.umontpellier.fr>
2022-01-10 09:08:25 +0100

    routeGet

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 5000 // this is very important
+const axios = require('axios');
 
 app.get('/recettes', function (req, res) {
+  const fetchUser1 = axios.get('https://tpnote-d015.restdb.io/rest/recettes',  {headers:
+     { 'x-apikey': '1680e4fc77cb7ce11a34c4435cbb86fe40e14'
+     }})
+  .then(result => res.send(result.data))
   res.send('Hello World!')
 })
 
